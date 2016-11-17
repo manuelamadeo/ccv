7c91c5f3dca94c34506eb8b477e528b5a6480083
manuelamadeo <manuelbuendia26@gmail.com>
2016-11-17 14:46:14 +0100

    new

---
+++ images/perfil/etapa12/lasvaleras/www.cronoescalada.com/js/functions.js
@@ -0,0 +1,39 @@
+
+function confirmDelete(delUrl) {
+  if (confirm("¿Seguro que deseas borrar el track?")) {
+    document.location = delUrl;
+  }
+}
+
+function confirmDeletePendiente(delUrl) {
+  if (confirm("¿Seguro que deseas borrar el track?")) {
+    document.location = delUrl;
+  }
+}
+
+function confirmDeleteComentario(delUrl) {
+  if (confirm("¿Seguro que deseas borrar el comentario?")) {
+    document.location = delUrl;
+  }
+}
+
+function confirmDeleteTag(delUrl) {
+  if (confirm("¿Seguro que deseas borrar el tag?")) {
+    document.location = delUrl;
+  }
+}
+
+function confirmDeleteBici(delUrl) {
+  if (confirm("¿Seguro que deseas borrar la bici?")) {
+    document.location = delUrl;
+  }
+}
+
+function confirmDeleteComponente(delUrl) {
+  if (confirm("¿Seguro que deseas borrar el componente?")) {
+    document.location = delUrl;
+  }
+}
+
+
+
